37d6421b4bc14ee9efe422e6eac0b72845045d52
erkarl <karl@karlranna.com>
2014-02-01 11:55:57 +0200

    Fix linting errors

--- app2.py
+++ app2.py
@@ -1,7 +1,6 @@
 # all the imports
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+from flask import Flask
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
